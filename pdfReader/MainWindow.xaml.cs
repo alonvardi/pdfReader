0f774fd0257dd39a94ede3c15e4554f704acfaa4
vardilon <alonv836@gmail.com>
2023-12-13 19:28:19 +0200

    basic talk and ocr using google vision api

--- pdfReader/MainWindow.xaml.cs
+++ pdfReader/MainWindow.xaml.cs
@@ -27,6 +27,10 @@ using static pdfReader.MainWindow;
 using Windows.Data.Pdf;
 using Windows.Storage.Streams;
 using System.Threading.Tasks;
+using Google.Cloud.Vision.V1;
+using static System.Net.Mime.MediaTypeNames;
+using Google.Cloud.TextToSpeech.V1;
+using System.Text;
 
 namespace pdfReader
 {
@@ -35,9 +39,13 @@ namespace pdfReader
 
         private int _currentPageIndex = 0;
         private PdfDocument _pdfDocument;
-
+        private List<Word> processedWords;
         public MainWindow()
         {
+            string credentialsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "key.json");
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialsPath);
+
             this.InitializeComponent();
         }
 
@@ -86,16 +94,31 @@ namespace pdfReader
                 await page.RenderToStreamAsync(stream);
                 BitmapImage bitmap = new BitmapImage();
                 await bitmap.SetSourceAsync(stream);
+
+                // Reset stream position to the beginning
+                stream.Seek(0);
+
+                // Read the stream into a byte array
+                byte[] imageBytes;
+                using (var streamReader = stream.AsStream())
+                {
+                    imageBytes = new byte[streamReader.Length];
+                    await streamReader.ReadAsync(imageBytes, 0, imageBytes.Length);
+                }
+
                 PdfImage.Source = bitmap;
+
+                await ocrWithGoogle(imageBytes);
             }
         }
+
 
         private async void PdfScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
         {
             if (PdfScrollViewer.VerticalOffset == 0 && _currentPageIndex > 0)
             {
                 // Scroll to the bottom of the new page
-                PdfScrollViewer.ChangeView(null, PdfScrollViewer.ScrollableHeight, null);
+                PdfScrollViewer.ChangeView(null, PdfScrollViewer.ScrollableHeight - 1, null);
 
                 // User scrolled to the top, go to the previous page
                 _currentPageIndex--;
@@ -107,7 +130,7 @@ namespace pdfReader
             {
 
                 // Scroll to the top of the new page
-                PdfScrollViewer.ChangeView(null, 0, null);
+                PdfScrollViewer.ChangeView(null, 1, null);
                 // User scrolled to the bottom, go to the next page
                 _currentPageIndex++;
                 await RenderPdfPageAsync(_currentPageIndex);
@@ -116,11 +139,152 @@ namespace pdfReader
             }
         }
 
+        private async Task ocrWithGoogle(byte[] imageBytes)
+        {
+            // Instantiates a client
+            var client = ImageAnnotatorClient.Create();
+
+            // Create a Google.Cloud.Vision.V1.Image from the byte array
+            var image = Google.Cloud.Vision.V1.Image.FromBytes(imageBytes);
+
+            // Performs text detection on the image file
+            var ocrResponse = await client.DetectDocumentTextAsync(image);
+            if (ocrResponse != null && ocrResponse.Text != null)
+            {
+
+                 processedWords = ProcessOcrResponse(ocrResponse);
+
+                // Now you can query 'processedWords' to find words at specific coordinates, etc.
+
+            }
+            else
+            {
+                Debug.WriteLine("No text found.");
+            }
+        }
+
+        private List<Word> ProcessOcrResponse(TextAnnotation response)
+        {
+            List<Word> words = new List<Word>();
+
+            foreach (var page in response.Pages)
+            {
+                foreach (var block in page.Blocks)
+                {
+                    foreach (var paragraph in block.Paragraphs)
+                    {
+                        foreach (var word in paragraph.Words)
+                        {
+                            var boundingBox = new Rectangle(word.BoundingBox.Vertices[0].X, word.BoundingBox.Vertices[0].Y, word.BoundingBox.Vertices[2].X, word.BoundingBox.Vertices[2].Y);
+                            Word newWord = new Word(string.Join("", word.Symbols.Select(s => s.Text)), boundingBox);
+                            words.Add(newWord);
+
+                            Debug.WriteLine(newWord.toString());
+                        }
+                    }
+                }
+            }
+
+            return words;
+        }
 
 
 
+        private async void PdfImage_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            var position = e.GetCurrentPoint(PdfImage).Position;
+            int x = (int)position.X;
+            int y = (int)position.Y;
+
+            Debug.WriteLine($"clicked on ({x},{y})");
+
+            var word = FindWordAtCoordinates(x, y);
+            if (word != null)
+            {
+                // speech
+                var segments = SegmentText(word.getText());
+                await SpeakText(segments);
+
+                Debug.WriteLine($"{word.getText()}");
+            }
+        }
+        private Word FindWordAtCoordinates(int x, int y)
+        {
+            if (processedWords == null)
+            {
+                Debug.WriteLine("ProcessedWords NULL!");
+                return null;
+            }
+
+            return processedWords.FirstOrDefault(word => IsPointInBoundingBox(word.getBoundingBox(), x, y));
+        }
 
 
+        private bool IsPointInBoundingBox(Rectangle boundingBox, int x, int y)
+        {
+            return x >= boundingBox.getX1() && x <= boundingBox.getX2() && y >= boundingBox.getY1() && y <= boundingBox.getY2();
+        }
+        static async Task SpeakText(List<(string Text, string LanguageCode)> segments)
+        {
+            foreach (var segment in segments)
+            {
+                await SynthesizeAndPlaySegment(segment.Text, segment.LanguageCode);
+            }
+        }
+
+        static async Task SynthesizeAndPlaySegment(string text, string languageCode)
+        {
+            TextToSpeechClient client = TextToSpeechClient.Create();
+            var input = new SynthesisInput { Text = text };
+            var voice = new VoiceSelectionParams
+            {
+                LanguageCode = languageCode,
+                SsmlGender = SsmlVoiceGender.Neutral
+            };
+            var config = new AudioConfig { AudioEncoding = AudioEncoding.Linear16 };
+            var response = await client.SynthesizeSpeechAsync(input, voice, config);
+
+            using (var stream = new MemoryStream(response.AudioContent.ToByteArray()))
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(stream);
+                player.PlaySync(); // Play each segment synchronously
+            }
+        }
+
+        public static List<(string Text, string LanguageCode)> SegmentText(string mixedText)
+        {
+            var segments = new List<(string Text, string LanguageCode)>();
+
+            // Simple heuristic: Split based on character set (e.g., Latin characters for English)
+            var words = mixedText.Split(' ');
+            StringBuilder currentSegment = new StringBuilder();
+            string currentLang = IsHebrew(words[0]) ? "he-IL" : "en-US";
+
+            foreach (var word in words)
+            {
+                string lang = IsHebrew(word) ? "he-IL" : "en-US";
+                if (lang != currentLang)
+                {
+                    segments.Add((currentSegment.ToString().Trim(), currentLang));
+                    currentSegment.Clear();
+                    currentLang = lang;
+                }
+                currentSegment.Append(word + " ");
+            }
+
+            if (currentSegment.Length > 0)
+            {
+                segments.Add((currentSegment.ToString().Trim(), currentLang));
+            }
+
+            return segments;
+        }
+
+        private static bool IsHebrew(string word)
+        {
+            // Implement a check to see if the word contains Hebrew characters
+            return word.Any(c => c >= 'à' && c <= 'ú');
+        }
 
     }
 }
---
+++ pdfReader/Word.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdfReader
+{
+    public class Word
+    {
+        private String Text;
+        private Rectangle BoundingBox;
+        public Word(String text, Rectangle rectangle)
+        {
+            Text = text;
+            BoundingBox = rectangle;
+        }
+        public String getText() { return Text; }
+        public void setText(String text) { Text = text; }
+        public Rectangle getBoundingBox() { return BoundingBox; }
+        public void setBoundingBox(Rectangle rectangle) { BoundingBox = rectangle; }
+        public String toString() { return Text + " " + BoundingBox.toString(); }
+    }
+
+    public class Rectangle
+    {
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+        public int getX1()
+        { return x1; }
+
+        public int getY1() { return y1; }
+        public int getX2() { return x2; }
+        public int getY2() { return y2; }
+        public void setX1(int x) { x1 = x; }
+        public void setY1(int y) { y1 = y; }
+        public void setX2(int x) { x2 = x; }
+        public void setY2(int y) { y2 = y; }
+        public Rectangle(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public String toString()
+        {
+            return $"{x1},{y1} -> {x2},{y2}";
+        }
+    }
+
+}
